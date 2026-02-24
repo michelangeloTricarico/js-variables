d2b1c9161100f678d4da96c0b27b41fd3d1c4c88
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-02-24 21:26:06 +0100

    Tipi primitivi

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -8,6 +8,10 @@ console.log(name)
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+let number=4
+let text= "Ciao"
+let bool= true
+console.log(number,text,bool)
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
